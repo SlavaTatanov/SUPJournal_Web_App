93a0756f0b73068222be884c87c809d1a939256d
SlavaTatanov <slavatatanov@gmail.com>
2022-11-30 22:26:42 +0500

    gpx tool

--- SUPJournal/tools/gpx.py
+++ SUPJournal/tools/gpx.py
@@ -2,14 +2,17 @@ import gpxpy
 import datetime
 import requests
 import json
+import folium
 
 
 class GpxFile:
     def __init__(self, file: str):
         self.__gpx = self.gpx_parser(file)
-        self.coord = self.get_coord(self.__gpx)
-        self.time = self.get_time(self.__gpx)
-        self.weather = self.get_weather(self.__gpx)
+        self.coord = self.get_coord()
+        self.time = self.get_time()
+        self.weather = self.get_weather()
+        self.med_point = self.get_med_point()
+        self.map = self.create_map()
 
     @staticmethod
     def gpx_parser(file):
@@ -17,37 +20,55 @@ class GpxFile:
             gpx = gpxpy.parse(gpx_file)
         return gpx
 
-    @staticmethod
-    def get_coord(gpx):
+    def get_coord(self):
         gpx_data = []
-        for tracks in gpx.tracks:
+        for tracks in self.__gpx.tracks:
             for segments in tracks.segments:
                 for point in segments.points:
                     gpx_data.append((point.latitude, point.longitude))
         return gpx_data
 
-    @staticmethod
-    def get_time(gpx):
-        start = datetime.datetime.strptime(str(gpx.tracks[0].segments[0].points[0].time)[:-6], "%Y-%m-%d %H:%M:%S")
-        end = datetime.datetime.strptime(str(gpx.tracks[-1].segments[-1].points[-1].time)[:-6], "%Y-%m-%d %H:%M:%S")
+    def get_time(self):
+        start = datetime.datetime.strptime(str(self.__gpx.tracks[0].segments[0].points[0].time)[:-6],
+                                           "%Y-%m-%d %H:%M:%S")
+        end = datetime.datetime.strptime(str(self.__gpx.tracks[-1].segments[-1].points[-1].time)[:-6],
+                                         "%Y-%m-%d %H:%M:%S")
         return end - start
 
-    @staticmethod
-    def __get_mid_time(gpx):
-        med = int(len(gpx.tracks[0].segments[0].points) / 2)
-        info = datetime.datetime.strptime(str(gpx.tracks[0].segments[0].points[med].time)[:-6], "%Y-%m-%d %H:%M:%S")
-        coord = (round(gpx.tracks[0].segments[0].points[med].latitude, 4),
-                 round(gpx.tracks[0].segments[0].points[med].longitude, 4))
+    def __get_mid_time(self):
+        med = int(len(self.__gpx.tracks[0].segments[0].points) / 2)
+        info = datetime.datetime.strptime(str(self.__gpx.tracks[0].segments[0].points[med].time)[:-6],
+                                          "%Y-%m-%d %H:%M:%S")
+        coord = (round(self.__gpx.tracks[0].segments[0].points[med].latitude, 4),
+                 round(self.__gpx.tracks[0].segments[0].points[med].longitude, 4))
         date = info.date()
         return {"index": info.hour, "coord": coord, "date": str(date)}
 
-    def get_weather(self, gpx):
-        data = self.__get_mid_time(gpx)
+    def get_weather(self):
+        data = self.__get_mid_time()
         req = requests.get(f"https://archive-api.open-meteo.com/v1/"
                            f"era5?latitude={data['coord'][0]}&longitude={data['coord'][1]}&"
                            f"start_date={data['date']}&end_date={data['date']}&hourly=temperature_2m")
         req = json.loads(req.text)
         return f"{req['hourly']['temperature_2m'][data['index']]} °C"
+
+    def get_med_point(self):
+        lat = [item[0] for item in self.coord]
+        lon = [item[1] for item in self.coord]
+        lat = {"min": min(lat), "max": max(lat)}
+        print(lat)
+        lon = {"min": min(lon), "max": max(lon)}
+        lat = round((lat["min"] + lat["max"]) / 2, 6)
+        lon = round((lon["min"] + lon["max"]) / 2, 6)
+        return [lat, lon]
+
+    def create_map(self):
+        my_map = folium.Map(location=self.med_point, zoom_start=14)
+        folium.vector_layers.PolyLine(self.coord).add_to(my_map)
+        return my_map
+
+    def save_map(self, file_name):
+        self.map.save(file_name)
 
 
 if __name__ == "__main__":
@@ -55,3 +76,5 @@ if __name__ == "__main__":
     print(test.time)
     print(test.weather)
     print(test.coord)
+    print(test.get_med_point())
+    test.save_map("map.html")
