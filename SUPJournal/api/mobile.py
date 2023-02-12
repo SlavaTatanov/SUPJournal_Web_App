300354aa8fe0791cd3cca4ce4630e162a7cf4d80
SlavaTatanov <slavatatanov@gmail.com>
2023-02-12 17:18:52 +0500

    mobile API

--- SUPJournal/api/mobile.py
+++ SUPJournal/api/mobile.py
@@ -1,13 +1,18 @@
-from flask import Blueprint
-from flask import jsonify
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import create_access_token
-from flask import request
 from SUPJournal.database.models import User
 from werkzeug.security import check_password_hash
 
 bp = Blueprint("api_mobile", __name__, url_prefix="/api/mobile")
 
+CONSTANCE = {"incorrect_user": "incorrect_user",
+             "incorrect_password": "incorrect_password",
+             "ok": "ok"}
+
 class Response:
+    """
+    Интерфейс ответа сервера на запрос авторизации
+    """
     def __init__(self, status: str, token: str = "", msg: str = ""):
         self.status = status
         self.token = token
@@ -24,8 +29,8 @@ def mobile_auth():
 
     query = User.query.filter_by(login=user).first()
     if query is None:
-        return Response("false", msg="Пользователь не существует").to_json()
+        return Response(CONSTANCE["incorrect_user"], msg="Пользователь не существует").to_json()
     elif not check_password_hash(query.pass_, password):
-        return Response("false", msg="Неверный пароль").to_json()
+        return Response(CONSTANCE["incorrect_password"], msg="Неверный пароль").to_json()
 
-    return Response("ok", create_access_token(query.login)).to_json()
+    return Response(CONSTANCE["ok"], token=create_access_token(query.login)).to_json()
