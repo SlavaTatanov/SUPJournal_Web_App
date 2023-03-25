3f8026c01aa0d8ce8401db1fdefa524b1362f8bd
SlavaTatanov <slavatatanov@gmail.com>
2023-03-25 09:49:49 +0500

    processing of the function that shows the marker on the map

--- SUPJournal/static/mapPoint.js
+++ SUPJournal/static/mapPoint.js
@@ -1,3 +1,18 @@
-function addPoint () {
-  L.marker([55.2, 60.1]).addTo(map_TrainingMap)
+var userPoint = null
+
+/*
+Показывает точку с заданными координатами,
+если данная точка не отображается на карте а координаты переданы то создается точка и показывается.
+В случае если точка создана но в координаты передается null точка убирается с карты.
+Если точка уже есть и передаются новые координаты точка перемещается.
+*/
+function showUserPoint (coord) {
+  if (userPoint == null && coord != null) {
+    userPoint = L.marker(coord).addTo(map_TrainingMap)
+  } else if (coord == null && userPoint != null) {
+    userPoint.remove()
+    userPoint = null
+  } else if (coord != null && userPoint != null) {
+    userPoint.setLatLng(coord).move()
+  }
 }
